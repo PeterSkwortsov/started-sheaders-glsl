ea195f87915a7ab5eab631029363cf24179bd738
Peter <skwortsow.petia@yandex.ru>
2025-04-03 13:00:32 +0300

    Движение частиц при смене модели (Старт)

--- src/script.js
+++ src/script.js
@@ -2,60 +2,29 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import GUI from "lil-gui";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import shadingVertexShader from "./shaders/shading/vertex.glsl";
-import shadingFragmentShader from "./shaders/shading/fragment.glsl";
-
+import shadingVertexShader from "./shaders/atmosphere/vertex.glsl";
+import shadingFragmentShader from "./shaders/atmosphere/fragment.glsl";
+import gsap from 'gsap'
+import { DRACOLoader } from "three/examples/jsm/Addons.js";
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI();
+const gui = new GUI({ width: 340 })
+const debugObject = {}
 
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 // Loaders
-const gltfLoader = new GLTFLoader();
-const textureLoader = new THREE.TextureLoader();
-
-const displacement = {}
-
-displacement.canvas = document.createElement('canvas');
-displacement.canvas.width = 128;
-displacement.canvas.height = 128;
-displacement.canvas.style.position = 'fixed'
-displacement.canvas.style.width = '256px'
-displacement.canvas.style.height = '256px'
-displacement.canvas.style.left = 0
-displacement.canvas.style.zIndex = 10
-document.body.append(displacement.canvas)
-
-displacement.context = displacement.canvas.getContext('2d')
-displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
-
-displacement.glowImage = new Image()
-displacement.glowImage.src = '/earth/glow.png'
-
-
-displacement.interactivePlane = new THREE.Mesh(
-  new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshBasicMaterial({
-    color: 'red',
-    side: THREE.DoubleSide
-  })
-)
-displacement.interactivePlane.visible = false
-scene.add(displacement.interactivePlane)
-
-displacement.raycaster = new THREE.Raycaster()
-displacement.screenCursor = new THREE.Vector2(9999, 9999)
-displacement.canvasCursor = new THREE.Vector2(9999, 9999)
-displacement.canvasCursorPrevious = new THREE.Vector2(9999, 9999)
-
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('./draco/')
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
 
 /**
  * Sizes
@@ -63,40 +32,38 @@ displacement.canvasCursorPrevious = new THREE.Vector2(9999, 9999)
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-  pixelRatio: Math.min(window.devicePixelRatio, 2),
-};
+  pixelRatio: Math.min(window.devicePixelRatio, 2)
+}
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
+  // Materials
+  particles.material.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
 
   // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
   // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(sizes.pixelRatio);
-});
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(sizes.pixelRatio)
+})
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  45,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-// camera.position.x = 7;
-// camera.position.y = 5;
-camera.position.z = 15;
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0, 0, 8 * 2)
+scene.add(camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
@@ -104,115 +71,53 @@ scene.add(camera);
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
-});
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 3;
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(sizes.pixelRatio);
+})
 
-// Enable shadows
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Optional, softer shadows
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(sizes.pixelRatio)
 
+debugObject.clearColor = '#160920'
+gui.addColor(debugObject, 'clearColor').onChange(() => { renderer.setClearColor(debugObject.clearColor) })
+renderer.setClearColor(debugObject.clearColor)
 
-const controls = new OrbitControls(camera, renderer.domElement);
+/**
+ * Particles
+ */
+const particles = {}
 
-displacement.texture = new THREE.CanvasTexture(displacement.canvas)
+// Geometry
+particles.geometry = new THREE.SphereGeometry(3)
+particles.geometry.setIndex(null)
 
-const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128);
-particlesGeometry.setIndex(null) // это параметр увеличивает производительность за счет передачи меньшего количества вершин
-particlesGeometry.deleteAttribute('normal')
- 
-
-const intensitiArray = new Float32Array(particlesGeometry.attributes.position.count) 
-const anglesArray = new Float32Array(particlesGeometry.attributes.position.count) 
-
-for (let i = 0; i < particlesGeometry.attributes.position.count; i++) {
-  intensitiArray[i] = Math.random()
-  anglesArray[i] = Math.random() * Math.PI * 2
-}
-
-particlesGeometry.setAttribute('aIntensity', new THREE.BufferAttribute(intensitiArray, 1))
-particlesGeometry.setAttribute('aAngle', new THREE.BufferAttribute(anglesArray, 1))
-
-const particlesMaterial = new THREE.ShaderMaterial({
+// Material
+particles.material = new THREE.ShaderMaterial({
   vertexShader: shadingVertexShader,
   fragmentShader: shadingFragmentShader,
-
-  uniforms: 
+  uniforms:
   {
-    uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
-    uPictureTexture: new THREE.Uniform(textureLoader.load('/earth/picture-1.png')),
-    uDisplacementTexture: new THREE.Uniform(displacement.texture)
-
+    uSize: new THREE.Uniform(0.4),
+    uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio))
   },
-  // blending: THREE.AdditiveBlending
-
+  blending: THREE.AdditiveBlending,
+  depthWrite: false
 })
 
-const particles = new THREE.Points(particlesGeometry, particlesMaterial)
-scene.add(particles)
-
-
-
-
-
-window.addEventListener('pointermove', (event) => {
-  displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1
-  displacement.screenCursor.y = -(event.clientY / sizes.height) * 2 + 1
-})
-
-
-
+// Points
+particles.points = new THREE.Points(particles.geometry, particles.material)
+scene.add(particles.points)
 
 /**
  * Animate
  */
-const clock = new THREE.Clock();
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+  // Update controls
+  controls.update()
 
-
-  controls.update();
-
-  displacement.raycaster.setFromCamera(displacement.screenCursor, camera)
-  const intersections = displacement.raycaster.intersectObject(displacement.interactivePlane)
-
-  if (intersections.length)
-  {
-    const uv = intersections[0].uv
-    displacement.canvasCursor.x = uv.x * displacement.canvas.width
-    displacement.canvasCursor.y = (1 - uv.y) * displacement.canvas.height
-  }
-
-  displacement.context.globalCompositeOperation = 'source-over'
-  displacement.context.globalAlpha = 0.02
-  displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
-
-  const cursorDistance = displacement.canvasCursorPrevious.distanceTo(displacement.canvasCursor)  
-  const alpha = Math.min(cursorDistance * 0.1, 1)
-
-  displacement.canvasCursorPrevious.copy(displacement.canvasCursor)
-
-  const glowSize = displacement.canvas.width * 0.25
-  displacement.context.globalCompositeOperation = 'lighten'
-  displacement.context.globalAlpha = alpha
-
-  displacement.context.drawImage(
-    displacement.glowImage,
-    displacement.canvasCursor.x - glowSize * 0.5,
-    displacement.canvasCursor.y - glowSize * 0.5,
-    glowSize,
-    glowSize
-  )
-
-  displacement.texture.needsUpdate = true
-  // Render
-  renderer.render(scene, camera);
+  // Render normal scene
+  renderer.render(scene, camera)
 
   // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
+  window.requestAnimationFrame(tick)
+}
 
-tick();
+tick()
--- src/shaders/shading/vertex.glsl
+++ src/shaders/shading/vertex.glsl
@@ -1,39 +1,18 @@
 uniform vec2 uResolution;
-uniform sampler2D uPictureTexture;
-uniform sampler2D uDisplacementTexture;
-
-attribute float aIntensity;
-attribute float aAngle;
-varying vec3 vColor;
+uniform float uSize;
 
 void main()
 {
 
-    vec3 newPosition = position;
-    float displacementIntensity = texture(uDisplacementTexture, uv).r;
-    displacementIntensity = smoothstep(0.1, 1.0, displacementIntensity);
-
-    vec3 displacement = vec3(
-        cos(aAngle), 
-        sin(aAngle),
-        1.0
-    );
-    displacement = normalize(displacement);
-    displacement *= displacementIntensity;
-    displacement *= 0.3;
-    displacement *= aIntensity;
-    displacement *= aAngle;
-    newPosition += displacement;
+   
 
     vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
 
-    float pictureIntensity = texture(uPictureTexture, uv).r;
 
-    gl_PointSize = 0.15 * pictureIntensity * uResolution.y;
+    gl_PointSize = uSize * uResolution.y;
     gl_PointSize *= (1.0 / -viewPosition.z);
 
-    vColor = vec3(pow(pictureIntensity, 2.0));
 }
